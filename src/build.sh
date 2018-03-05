01fcfede7257bac1aed8bc43c69417c44a9efc07
Adriano Gil <adrianogil.san@gmail.com>
2018-03-05 07:42:13 -0400

    Improved build script

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,5 @@
-pdflatex sbc-template.tex && bibtex sbc-template && pdflatex sbc-template.tex
+pdflatex sbc-template.tex
+bibtex sbc-template
+pdflatex sbc-template.tex
+bibtex sbc-template
+pdflatex sbc-template.tex
